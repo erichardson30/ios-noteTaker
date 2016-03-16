2ceddaf64c69c5c668018eb4790d2ee4a03885c3
Eric <eric.richardson30@gmail.com>
2016-03-16 13:45:20 -0400

    each table row is playing a set mp3 file

--- noteTaker/NoteTakerViewController.swift
+++ noteTaker/NoteTakerViewController.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import AVFoundation
 
 class NoteTakerViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var audioPlayer = AVAudioPlayer()
 
     @IBOutlet weak var tableView: UITableView!
     
@@ -34,7 +37,24 @@ class NoteTakerViewController: UIViewController, UITableViewDataSource, UITableV
         cell.textLabel!.text = "The Notes"
         return cell;
     }
+    
+    func getAudioPlayerFile(file: String, type:String) -> AVAudioPlayer {
+        let path = NSBundle.mainBundle().pathForResource("SampleAudio", ofType: "mp3")
+        let url = NSURL.fileURLWithPath(path!)
+        var audioPlayer:AVAudioPlayer?
+        
+        do {
+            try audioPlayer = AVAudioPlayer(contentsOfURL: url)
+        } catch let audioPlayerError as NSError {
+            print("Failed to find the file: \(audioPlayerError.localizedDescription)")
+        }
+        
+        return audioPlayer!
+    }
 
-
+    func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
+        audioPlayer = getAudioPlayerFile("StartRecordSound", type: "m4a")
+        audioPlayer.play()
+    }
 }
 
