9d44f6fe13e97dc6382d0bc638a7c408f9f82ec5
Eric <eric.richardson30@gmail.com>
2016-03-16 13:31:28 -0400

    basic table view set up

---
+++ noteTaker/NoteTakerViewController.swift
@@ -0,0 +1,40 @@
+//
+//  NoteTakerViewController
+//  noteTaker
+//
+//  Created by Richardson, Eric on 3/16/16.
+//  Copyright © 2016 Richardson, Eric. All rights reserved.
+//
+
+import UIKit
+
+class NoteTakerViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 9
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel!.text = "The Notes"
+        return cell;
+    }
+
+
+}
+
